d1185c15708da5a106ad059808597c9c8f176a28
indpyamamoto2403 <yamamoto@indp.co.jp>
2024-10-09 05:58:15 +0000

    レイアウト微修正

--- frontend/resources/js/Pages/Dashboard.vue
+++ frontend/resources/js/Pages/Dashboard.vue
@@ -7,24 +7,20 @@ import { Head } from '@inertiajs/vue3';
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
-                Dashboard
-            </h2>
-        </template>
-
-        <div class="py-12">
+        <div class="min-h-screen py-12 bg-gradient-to-b from-blue-500 to-black">
+            
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+                    class="overflow-hidden bg-black shadow-sm sm:rounded-lg"
                 >
-                    <div class="p-6 text-gray-900">
-                        You're logged in!
+                    <div class="p-6 text-white">
+                        Moon Portal にログインしました。
                     </div>
                 </div>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
+
+<style scoped>
+</style>
